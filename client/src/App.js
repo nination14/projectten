da0a679bfc36a6cb83701b04c8507c8933f41452
ahirina rivera <ahirina.rivera@verizon.com>
2019-10-07 13:59:21 -0500

    Called Rest API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    courses: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/courses')
+      .then(response => response.json())
+      .then(courses => { 
+        this.setState({courses});
+      });
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <ul>
+            { this.state.courses.map( course => <li>{ course.title }</li>) }
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
